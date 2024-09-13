1e0a0cd2bdea167d9d308bc5312afdc29c110121
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-09-13 17:46:37 +0200

    Ajout des elements bootstrap et autres parametrage pour le css + navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import "bootstrap"
+import "@popperjs/core"
 
 const application = Application.start()
 application.debug = false
