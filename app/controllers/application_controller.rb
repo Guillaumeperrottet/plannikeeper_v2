c1a0f3d9821de2d38c8935c0b7b16b96ac9c9afb
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-09-15 10:32:00 +0200

    controller de base modifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_path
+
+  private
+
+  def set_current_path
+    @current_path = case request.path
+                    when root_path then "Home"
+                    when new_objet_path then "Créer un objet"
+                    when edit_objet_path then "Modifier un objet"
+                    when objet_path(params[:id]) then "Objet : #{@objet.nom}"
+                    when objets_path then "Objets"
+                    # Ajoute d'autres chemins si nécessaire
+                    else "Page"
+                    end
+  end
 end
