ad385a5dec55224fde4ab79f4ad98fcfb6318a89
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-09-22 17:47:00 +0200

    Ajout de react.js et installation controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+// import ReactController from "./controllers/react_controller";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import "bootstrap"
 import "@popperjs/core"
+
 
 const application = Application.start()
 application.debug = false
