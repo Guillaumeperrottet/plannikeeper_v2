2950f189d885e3d8a96b6e1c52062103b5b761a1
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-06 17:22:13 +0200

    a

--- db/migrate/20241006150004_add_devise_to_users.rb
+++ db/migrate/20241006150004_add_devise_to_users.rb
@@ -3,38 +3,18 @@
 class AddDeviseToUsers < ActiveRecord::Migration[7.1]
   def self.up
     change_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      ## Devise modules
+      ## Don't add email if it already exists
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(:users, :encrypted_password)
+      t.string :reset_password_token unless column_exists?(:users, :reset_password_token)
+      t.datetime :reset_password_sent_at unless column_exists?(:users, :reset_password_sent_at)
+      t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
+      t.string :confirmation_token unless column_exists?(:users, :confirmation_token)
+      t.datetime :confirmed_at unless column_exists?(:users, :confirmed_at)
+      t.datetime :confirmation_sent_at unless column_exists?(:users, :confirmation_sent_at)
+      t.string :unconfirmed_email unless column_exists?(:users, :unconfirmed_email)
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      # If you have other Devise-related fields, make sure to check them here as well
     end
 
     add_index :users, :email,                unique: true
--- db/migrate/20241006151206_create_users.rb
+++ db/migrate/20241006151206_create_users.rb
@@ -1,10 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
-  def change
-    create_table :users do |t|
-      t.string :email, null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.timestamps null: false
-    end
+  change_table :users do |t|
+    ## Devise modules
+    ## Don't add email if it already exists
+    t.string :encrypted_password, null: false, default: "" unless column_exists?(:users, :encrypted_password)
+    t.string :reset_password_token unless column_exists?(:users, :reset_password_token)
+    t.datetime :reset_password_sent_at unless column_exists?(:users, :reset_password_sent_at)
+    t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
+    t.string :confirmation_token unless column_exists?(:users, :confirmation_token)
+    t.datetime :confirmed_at unless column_exists?(:users, :confirmed_at)
+    t.datetime :confirmation_sent_at unless column_exists?(:users, :confirmation_sent_at)
+    t.string :unconfirmed_email unless column_exists?(:users, :unconfirmed_email)
+
+    # If you have other Devise-related fields, make sure to check them here as well
+  end
 
     add_index :users, :email, unique: true
   end
