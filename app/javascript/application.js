15e99482d4849d1ddb19398dbcd1503a520643c8
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-06 14:20:55 +0200

    turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@ import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import "bootstrap"
 import "@popperjs/core"
+import "@hotwired/turbo-rails"
 
 const application = Application.start()
 application.debug = false
