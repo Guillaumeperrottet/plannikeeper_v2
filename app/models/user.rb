527dd65987cd36c64df06043f3a67ffb15aeb023
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-11 09:31:53 +0200

    ok pas trop mal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+
   has_one_attached :avatar
   has_many :objets, dependent: :destroy
   devise :database_authenticatable, :registerable,
