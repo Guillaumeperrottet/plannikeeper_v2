fd90ac0a120f410534059eeda3999e417d4bce91
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-12-16 19:23:42 +0100

    modif permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,11 +10,13 @@ class Ability
       can :manage, Task, article: { secteur: { objet: { user_id: user.id } } }
     elsif user.enterprise_admin?
       # Admin entreprise peut tout gérer dans son entreprise
-      can :manage, Objet, company_id: user.company_id
-      can :manage, Task, article: { secteur: { objet: { company_id: user.company_id } } }
+      can :manage, :all, company_id: user.company_id
 
       # Gestion des utilisateurs de l'entreprise
       can :manage, User, company_id: user.company_id
+
+      # Permet de gérer les tâches liées à un article appartenant à la même entreprise
+      can :manage, Task, article: { secteur: { objet: { company_id: user.company_id } } }
     elsif user.enterprise_user?
       # Utilisateur entreprise : accès limité
       can :read, Task, article: { secteur: { objet: { company_id: user.company_id } } }
