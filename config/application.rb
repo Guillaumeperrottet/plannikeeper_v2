2e84ad371dc259eb2b779e9a0168915169702173
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2025-01-06 15:04:55 +0100

    gem devise de EN a FR

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,10 @@ module Plannikeeper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    # Définir la langue par défaut
+    config.i18n.default_locale = :fr
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration supplémentaire
+    config.autoload_lib(ignore: %w(assets tasks))
   end
 end
